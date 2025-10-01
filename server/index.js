1fb32e7086f22f622a836adf78c5e1b5bb6544e8
Kameron Carter <kamcarter@claflin.edu>
2025-10-01 13:25:57 -0400

    Signup & Login w/o noti messages

--- server/index.js
+++ server/index.js
@@ -9,10 +9,26 @@ app.use(cors());
 
 mongoose.connect("mongodb://127.0.0.1:27017/user");
 
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    userModel.findOne({ email: email })
+        .then(user => {
+            if (user) {
+                if (user.password === password) {
+                    res.json("Login Successful");
+                } else {
+                    res.json("Invalid Credentials");
+                }
+            } else {
+                res.json("User not registered");
+            }
+        })
+})
+
 app.post('/register', (req, res) => {
     userModel.create(req.body)
-    .then(users => res.json(users))
-    .catch(err => res.json(err))
+        .then(users => res.json(users))
+        .catch(err => res.json(err))
 })
 
 app.listen(3001, () => {
