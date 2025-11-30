23e3c03fb225dec62e2385661b5ec4e69d8d2a95
Kameron Carter <kamcarter@claflin.edu>
2025-11-29 20:18:25 -0500

    Partial Information uploaded

--- client/src/pages/Budgeting.jsx
+++ client/src/pages/Budgeting.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import Navbar from '../components/Navbar.jsx';
 import './Pages.css';
 
+// Ideas
+// Make the quizzes at the end of each page pop out at the user
+// Add more engaging graphics and images to the pages
+// Add something for auditory learners like videos or podcasts
+//   For example an AI voice to speak on the aside key takeaways
+
 function Budgeting() {
     return (
         <>
             <div className="site-background">
-                <div className="d-flex justify-content-center align-items-center vh-100 ">
+                <div className="d-flex justify-content-center align-items-center ">
                     <div className="site-shape">
                         <header className="header">
                             <h1>FinanceMadeSimple</h1>
@@ -19,36 +25,69 @@ function Budgeting() {
                         <div className="main-container">
                             <section className="content">
 
-                                <h4>Introduction to Financial Literacy</h4>
+                                <h4>Importance of Budgeting</h4>
 
                                 <p className="left-section">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
+                                    About 60% of Americans can't cover a $1,000 emergency expense with savings and 80% struggle with debt. This highlights 
+                                    the need for effective budgeting skills to manage finances and build savings. Budgeting helps individuals track income and expenses so that 
+                                    they don't spend more than they earn. <strong> The correct budget can help reduce debt, increase savings, prepare for emergencies, 
+                                    and achieve financial goals.</strong>
                                 </p>
                                 <hr />
 
-                                <h4 className="right-section">More Overview</h4>
+                                <h4 className="right-section">How to Create a Budget</h4>
 
                                 <p className="right-section">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
+                                    To create a budget, start by <strong>gaining all financial statements</strong>, whether that be bank statements, bills, or pay stubs. Next, 
+                                    <strong>list all sources of income and all monthly expenses</strong>, including fixed and variable costs (meaning costs that stay the same each month and 
+                                    cost the can change). <strong>Add all incomes and expenses to determine what money is left over or needed</strong>. Once the results are gathered, determine
+                                    your next course of action. If your expenses are higher than your income, look for areas to cut back. If you have money left over, consider allocating 
+                                    it towards savings or debt repayment.
                                 </p>
                                 <hr />
+
+                                <h4>Types of Budgets</h4>
+
+                                <p className="left-section">
+                                    There are several types of budgets that individuals can used based on their financial situation, goals and preferences.
+                                    Some common types include the <strong>traditional budget,  the 50/30/20 budget, the zero-based-budget, and the envelope system</strong>. The traditional budget
+                                    involves tracking income and expense only to prevent overspending. The 50/30/20 budget divides income into three categories: 50% for needs, 
+                                    30% for wants, and 20% for savings and debt repayment. The zero-based-budget entails making sure expenses subtracted from income equal zero at 
+                                    the end of the month. The envelope system is a cash system that has you make an envelope for each category you create and stop spending once the 
+                                    envelope is empty.
+                                    </p>
+
+                                <h4 className="right-section">Tips for Successful Budgeting</h4>
+
+                                <p className="right-section">
+                                    Don't confuse necessities and luxuries, for example, a $10 meal vs a $50 meal. Make note of the small spending done throughout the month, those 
+                                    small drinks can add up quick! Try an use cash because even though credit and debit cards are convient they are easier to overuse because you can't
+                                    see the money. Last but not least, manage your debt by paying off loans or credit cards and seek help if it becomes too much (it is okay to ask for
+                                    help!).
+                                </p>
+                                <hr />
+
+                                <footer className="footer">
+                                    <h6>Learn more about budgeting below</h6>
+                                    <p>
+                                        <ul>
+                                            <li><a href="https://www.incharge.org/financial-literacy/budgeting-saving/how-to-make-a-budget/">InCharge - Budgeting</a></li>
+                                            
+                                        </ul>
+                                    </p>
+                                </footer>
+
 
                             </section>
 
                             <aside className="sidebar">
-                                <h5>Key Takeaways</h5>
+                                <h5>Quick Learning</h5>
                                 <ul>
-                                    <li>Understanding the basics of personal finance is crucial.</li>
-                                    <li>Budgeting helps in managing expenses effectively.</li>
-                                    <li>Saving and investing early can lead to financial security.</li>
+                                    <li>Budgeting is important to ensure no overspending and preparation to emergencies</li>
+                                    <li>Create a budget by gaining and adding incomes and expenses then use a budget type with said information</li>
+                                    <li>Different types of budgets based on goals, financial situation, and preferences</li>
+                                    <li>Don't confuse needs and wants</li>
+                                    <li>Manage debts and ask for help when needed</li>
                                 </ul>
                             </aside>
                         </div>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     return (
         <>
             <div className="site-background">
-                <div className="d-flex justify-content-center align-items-center vh-100 ">
+                <div className="d-flex justify-content-center align-items-center ">
                     <div className="site-shape">
                         <header className="header">
                             <h1>FinanceMadeSimple</h1>
@@ -22,33 +22,60 @@ function Home() {
                                 <h4>Introduction to Financial Literacy</h4>
 
                                 <p className="left-section">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
+                                    To begin understanding what financial literacy is, we must first define it. <strong>Financial literacy is the ability to understand and 
+                                    effectively use various financial skills</strong>, including budgeting, investing, borrowing, taxation, and personal financial management. We can split it into 
+                                    <strong> three main components: financial knowledge, financial behavior, and financial attitude.</strong> Each component plays a part in
+                                    shaping an individual's overall financial literacy. When COVID hit the United States, we strayed away from the use of cash
+                                    and moved towards a more digital economy. This shift has made it even more crucial for individuals to be financially literate which
+                                    is why this site aims to provide its users with the necessary knowledge on personal finance topics to improve financial literacy. Even though
+                                    this site is targeted towards college students, financial literacy is important for people of all ages.
                                 </p>
                                 <hr />
 
-                                <h4 className="right-section">More Overview</h4>
+                                <h4 className="right-section">Three Components</h4>
 
                                 <p className="right-section">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
+                                    The first component, <strong>financial knowledge</strong>, refers to an individual's understanding of financial concepts and products.
+                                    The next component, <strong>financial behavior</strong>, refers to the actions individuals take regarding their finances, such as budgeting,
+                                    saving, and investing. Last but not least, <strong>financial attitude</strong>, refers to an individual's mindset and beliefs about money and finances.
+                                    If you are knowledgeable about personal finance topics, practice good financial habits, and have a positive attitude towards money management,
+                                    you are more likely to make informed financial decisions and achieve financial well-being. Each page on this site will cover the main topics
+                                    that contribute to financial literacy including savings, managing credit, debt, & loans, and how to budget effectively. 
                                 </p>
                                 <hr />
+
+                                <h4>The Importance of Financial Literacy in College Students</h4>
+                                <p className="left-section">
+                                    Financial literacy equips individuals with the knowledge to make informed financial decisions, contributing to personal and economic stability. 
+                                    <strong>Without the knowledge embedded in financial literacy, an individual will have a hard time navigating the economy.</strong>
+                                    This has become a global realization with the focus being shifted towards the youth which is, according to the UN, ages 15-24. This age group
+                                    can most benefit from financial literacy education due to the fact that college debt is at an all time high. Being financially literate can help
+                                    you navigate the complexities of student loans, credit cards, and budgeting for the first time. It can also help you avoid common financial pitfalls
+                                    like overspending on things like food, entertainment, and clothing. <strong>By improving your personal finance skills you can set yourself up for a 
+                                    successful financial future right after college.</strong>
+                                </p>
+
+                                <footer className="footer">
+                                    <h6>Learn more about financial literacy below</h6>
+                                    <p>
+                                        <ul>
+                                            <li><a href="https://corporatefinanceinstitute.com/resources/wealth-management/financial-literacy/">Corporate Finance Institute - Financial Literacy</a></li>
+                                            <li><a href="https://www.investopedia.com/terms/f/financial-literacy.asp">Investopedia - Financial Literacy</a></li>
+                                            <li><a href="https://moneyzine.com/personal-finance/financial-literacy-statistics/">MoneyZine - Financial Literacy Statistics</a></li>
+                                        </ul>
+                                    </p>
+                                </footer>
 
                             </section>
 
                             <aside className="sidebar">
-                                <h5>Key Takeaways</h5>
+                                <h5>Quick Learning</h5>
                                 <ul>
-                                    <li>Understanding the basics of personal finance is crucial.</li>
-                                    <li>Budgeting helps in managing expenses effectively.</li>
-                                    <li>Saving and investing early can lead to financial security.</li>
+                                    <li>Financial literacy is the ability to understand and effectively use various financial skills</li>
+                                    <li>Financial literacy consists of three main components: financial knowledge, financial behavior, and financial attitude.</li>
+                                    <li>Understanding the three main components can lead to better financial decision-making.</li>
+                                    <li>College Students can set themselves up for financial success after college by being financially literate.</li>
+                                    <li>Financial literacy is a lifelong journey, and it's never too late to start learning.</li>
                                 </ul>
                             </aside>
                         </div>
